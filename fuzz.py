4ce49d0969a09cbb8fe55d8ed77b5e006f664080
Nords Zamora <100557534+fafsnords@users.noreply.github.com>
2022-06-08 23:41:59 -1200

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -83,7 +83,7 @@ class FuzzTester:
 
 fuzz = FuzzTester(0, 0, 0, 0, 0)
 
-column = open('/column/column.txt', 'r') # sql injection list of column
+column = open('column/column.txt', 'r') # sql injection list of column
 
 # sql injection exact vulnerable column list
 check = ['1'*5,'2'*5,'3'*5,'4'*5,'5'*5,'6'*5,'7'*5,'8'*5,'9'*5,'10'*3,'11'*3,'12'*3,'13'*3,'14'*3,'15'*3,'16'*3,'17'*3,'18'*3,'19'*3,'20'*3,'21'*3,'22'*3,'23'*3,'24'*3,'25'*3,'26'*3,'27'*3,'28'*3,'29'*3,'30'*3,'31'*3,'32'*3,'33'*3,'34'*3,'35'*3,'36'*3,'37'*3,'38'*3,'39'*3,'40'*3,'41'*3,'42'*3,'43'*3,'44'*3,'45'*3,'46'*3,'47'*3,'48'*3,'49'*3,'50'*3]
